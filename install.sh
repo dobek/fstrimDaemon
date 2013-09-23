b7a6b8d0f3f338ff6e1c3a70145adafe399ce57e
Dobek <dobek@users.noreply.github.com>
2013-09-22 21:15:17 -0700

    adding install script

---
+++ install.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+DIR=`dirname $0`
+
+cp -fv usr/sbin/fstrimDemon.sh /usr/sbin/fstrimDemon.sh
+RES=$?
+if [ "$RES" != "0" ]; then
+	echo Must be root to install fstrimDemon
+	exit $RES
+fi
+chmod 755 /usr/sbin/fstrimDemon.sh
+
+cp -fv etc/conf.d/fstrimDemon /etc/conf.d/fstrimDemon
+
+cp -fv etc/init.d/fstrimDemon /etc/init.d/fstrimDemon
+chmod 755 /etc/init.d/fstrimDemon
+
+echo
+echo Find more information in README.md
+echo
